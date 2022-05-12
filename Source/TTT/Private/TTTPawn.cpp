3475b0781444f30de7f63aabd5f49e7630fc41bc
sy <syao.y40@gmail.com>
2022-05-12 19:47:57 -0400

    Initial commit.

---
+++ Source/TTT/Private/TTTPawn.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TTTPawn.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ATTTPawn::ATTTPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	Camera->SetupAttachment(Root);
+	Camera->SetRelativeTransform(FTransform(FRotator(180.f, -90.f, 180.f),FVector(0.f, 0.f, 1000.f),FVector(1.f, 1.f, 1.f)));
+
+}
+
+// Called when the game starts or when spawned
+void ATTTPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATTTPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATTTPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	//Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
